7431f81efbb23099b5f572d40ef8cb5dde19366d
Adrien Rahier <adrien.rahier@gmail.com>
2015-03-03 00:09:17 +0100

    Update du Readme + On wrappe le code Hull dans un angular $apply de manière à forcer le rafraichissement de la vue.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Twitter-list
-A side project based on Hull and AngularJS to manage your list in Twitter
+
+Objectif :
+
+Contruire une application pour trier ses following Twitter en différentes listes.
+
+Technos :
+
+- AngularJS
+- Hull.io
+- Twitter API.
+
+Features :
+
+- Authentification de l'utilisateur à l'aide de Hull.
+- Récupération et affichage de ses listes Twitter via l'API (doit être présent : nombre de membres déjà dans la liste).
+- Tri des following n'ayant pas encore été mis dans une liste.
+- Pouvoir ajouter ces following à une liste.
+
+Bonus :
+
+- Récupération et affichage rapide des bios + ses deux-trois derniers tweets.
+- Présentation en mode "Tinder" des following.
+- Être capable de créer de nouvelles listes directement depuis l'app.
--- js/myscripts.js
+++ js/myscripts.js
@@ -18,31 +18,27 @@ var twitterListApp = angular.module("twitterListApp", []);
    }
 });*/
 
-
 twitterListApp.controller('TweetCtrl', ['$scope', function($scope){
    var TweetCtrl = this;
-
+   $scope.name = "there";
    $scope.go = function() {
-      var testName = "there";
-      $scope.name = testName;
       Hull.login({provider:'twitter'}).then(function(user) {
-         
-         testS(user.name);
-         
+        $scope.displayUsername(user.name);
       }, function(error) {
         console.log(error.message);
       });
    }
-   $scope.testS = function(newName) {
-      debugger;
-      $scope.name = newName;
+   $scope.displayUsername = function(hop) {
+      $scope.$apply(function () {
+         $scope.name = hop;
+     });
    }
    $scope.displayTweets = function() {
       Hull.api({
         provider:'twitter',
         path:'/statuses/home_timeline'
-      },function(result){
-        console.log('This is the result', result);
+      }, function(result){
+        console.log('This is the TL', result);
         for (var i = result.length - 1; i >= 0; i--) {
            TweetCtrl.tweets = result[i];
         };
