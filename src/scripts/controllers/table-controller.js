74321c1cd504c795677b96fdbd4d692955a23f07
Adrien Rahier <adrien.rahier@gmail.com>
2015-11-24 16:48:12 +0100

    adding animations on the table when subscribing a new user

--- src/scripts/controllers/table-controller.js
+++ src/scripts/controllers/table-controller.js
@@ -1,9 +1,16 @@
 'use strict';
 
 angular.module('twitterListApp')
-.controller('TableCtrl', ['$scope', 'TableService', 'PaginationService', '$timeout', function($scope, TableService, PaginationService, $timeout) {
-	$scope.handleInputClick = function(infos) {
+.controller('TableCtrl', ['$scope', '$timeout' ,'TableService', 'PaginationService', function($scope, $timeout, TableService, PaginationService) {
+	$scope.handleInputClick = function(infos, $event) {
 		infos.actionTodo = (infos.subscribed) ? "create" : "destroy";
 		TableService.addAction(infos);
+		var currentTarget = $($event.currentTarget);
+		currentTarget.attr("disabled", true);
+		currentTarget.parent().addClass("clicked");
+		$timeout(function() {
+			currentTarget.attr("disabled", false);
+			currentTarget.parent().removeClass("clicked");
+		}, 2500);
 	};
 }]);
